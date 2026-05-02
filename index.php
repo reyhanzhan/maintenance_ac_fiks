ef72fe94975bbe04f47b0ee2c0cc9da6b2f37852
reyhan <reyhanrizqi01@gmail.com>
2026-05-02 09:08:10 +0700

    deploy

--- index.php
+++ index.php
@@ -5,16 +5,24 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Support both layouts:
+// 1) document root at project root
+// 2) document root at public-like folder where project is one level up
+$basePath = __DIR__;
+if (! file_exists($basePath.'/vendor/autoload.php') && file_exists($basePath.'/../vendor/autoload.php')) {
+    $basePath = dirname(__DIR__);
+}
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require $basePath.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $basePath.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
